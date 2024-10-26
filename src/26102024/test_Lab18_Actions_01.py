ee9a962d7a6adc4537d91604fd8fd0d6054b5f8b
PrashantTurate <turate.prashant@gmail.com>
2024-10-26 22:47:42 +0530

    test_Lab18_Actions created

---
+++ src/26102024/test_Lab18_Actions_01.py
@@ -0,0 +1,30 @@
+import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+@allure.title("Actions P1")
+@allure.description("Verify keyboard actions")
+def test_verify_action_keyboard():
+    driver = webdriver.Chrome()
+    driver.get("https://awesomeqa.com/practice.html")
+
+    first_name = driver.find_element(By.XPATH, "//input[@name='firstname']")
+
+    # KEY_DOWN
+    actions = ActionChains(driver=driver)
+    (actions
+     .key_down(Keys.SHIFT)
+     .send_keys_to_element(first_name, "the testing academy")
+     .key_up(Keys.SHIFT).perform()
+     )
+
+    time.sleep(5)
+    driver.quit()
